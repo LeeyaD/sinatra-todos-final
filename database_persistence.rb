3aaa59d8d0fd929aed694aa46197216274ca3364
Leeya Davis <leeya463@gmail.com>
2022-08-20 19:16:47 -0400

    corrected relation name list > lists in sql file

--- database_persistence.rb
+++ database_persistence.rb
@@ -3,10 +3,10 @@ require "pg"
 class DatabasePersistence
   def initialize(logger)
     @db = if Sinatra::Base.production?
-            PG.connect(ENV['DATABASE_URL'])
-          else
-            PG.connect(dbname: "todos")
-          end
+      PG.connect(ENV['DATABASE_URL'])
+      else
+      PG.connect(dbname: "todos")
+    end
     @logger = logger
   end
 
