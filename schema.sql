d6108f90e30190cd1857b86d428498de09bb3c89
Leeya Davis <leeya463@gmail.com>
2022-08-20 17:23:25 -0400

    commiting project to deploy to Heroku

---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE list (
+  id serial PRIMARY KEY,
+  name text NOT NULL UNIQUE
+);
+
+CREATE TABLE todos (
+  id serial PRIMARY KEY,
+  name text NOT NULL,
+  completed boolean NOT NULL DEFAULT false,
+  list_id int NOT NULL,
+  FOREIGN KEY (list_id) REFERENCES list(id)
+);
